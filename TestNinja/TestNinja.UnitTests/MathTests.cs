98bde9bc0bd9df937ecbd4702371276cc3fd9970
Ivica Mamic <mamic.ivica@gmail.com>
2018-06-17 22:36:09 +0200

    add MathTests.cs

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSAumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+    }
+}
